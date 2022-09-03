8c8b108fd8050f4af2fa7da86367b4d80108ac53
juchan <jjc6252498@gmail.com>
2022-09-03 14:10:11 +0900

    조이스틱 회전 버그 수정
    조이스틱 회전 버그 수정
    조이스틱 회전을 시작할 때 고정 값에서 시작하는 버그가 있었다.
    고쳤음.

--- Assets/Scripts/RightVirtualJoystick.cs
+++ Assets/Scripts/RightVirtualJoystick.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems; // 키보드, 마우스, 터치를 이벤트로 오브젝트에 보낼 수 있는 기능을 지원
 
@@ -50,17 +51,27 @@ public class RightVirtualJoystick : MonoBehaviour, IBeginDragHandler, IDragHandl
     private void ControlJoystickLever(PointerEventData eventData)
     {
         //var inputPosition = eventData.position - joystick.anchoredPosition;
-
+        // Debug.Log("joystick.rect.center" + joystick.rect.center);
         // 앵커 우하단이라 코드 rect.center로 수정할려고 했으나
         // rect.center가 앵커위치 기준이라 -150 150이 나와서
-        // 스크린 사이즈 + 앵커에서 -x 만큼 떨어진 값인 rect x
         // 앵커에서 y만큼 떨어진 값인 rect y를 벡터에 넣었다.
-        var rectPos = new Vector2(Screen.width + joystick.rect.x , joystick.rect.y) ;
+        // 스크린 사이즈 + 앵커에서 -x 만큼 떨어진 값인 rect x - 너비만큼
+        // * 지금 rect 위치가 우측 하단에 잡혀있어서 너비와 높이 반만큼을 더해줘야함 (x축으로는 -이니까 빼줬음)
+        var joystickRectX = joystick.position.x - joystick.rect.width / 2;
+        var joystickRectY = joystick.position.y + joystick.rect.height / 2;
+        // rect.center -150 150
+        // joystick.position 월드 스크린 좌표
+        // eventData.position 월드 스크린 좌표
+        
+        var rectPos = new Vector2(joystickRectX, joystickRectY) ;
         var inputPosition = eventData.position - rectPos;
         //normalized : 백터 길이 1로 정규화, 이동 속도 일정
-        Debug.Log("eventData.position : " + eventData.position); 
-        Debug.Log("joystick.rect.center : " + joystick.rect.center); 
+        /*Debug.Log("eventData.position : " + eventData.position); 
+        Debug.Log("joystick.rect.center : " + joystick.rect.center);*/
+        // Debug.Log("inputPosition.magnitude" + inputPosition.magnitude); 
+        // Debug.Log("inputPosition" + inputPosition);
         var inputVector = inputPosition.magnitude < leverRange ? inputPosition : inputPosition.normalized * leverRange;
+        // Debug.Log("lever.anchoredPosition" + lever.anchoredPosition);
         lever.anchoredPosition = inputVector;
         inputDirection = inputVector / leverRange; //inputVector는 해상도 기반이라 값이 매우 커 정규화 함
     }
