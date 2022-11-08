b17a5e729c43072f68e8c7c20d69e626f3768a2a
juchan <jjc6252498@gmail.com>
2022-11-08 11:52:02 +0900

    UI 스케일 수정본
    UI 스케일 수정본

--- Assets/Scripts/Resolution.cs
+++ Assets/Scripts/Resolution.cs
@@ -1,11 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class thisCanvas : MonoBehaviour
+public class Resolution : MonoBehaviour
 {
-    //Default 해상도 비율
+    void Awake()
+    {
+        Camera camera = GetComponent<Camera>();
+        Rect rect = camera.rect;
+        float scaleHeight = ((float)Screen.width / Screen.height) / ((float)20 / 9);
+        float scaleWidth = 1f / scaleHeight;
+        if (scaleHeight < 1)
+        {
+            rect.height = scaleHeight;
+            rect.y = (1f - scaleHeight) / 2f;
+        }
+        else
+        {
+            rect.width = scaleWidth;
+            rect.x = (1f - scaleWidth) / 2f;
+        }
+
+        camera.rect = rect;
+    }
+
+    void OnPreCull()
+    {
+        GL.Clear(true, true, Color.black);
+    }
+    /*//Default 해상도 비율
     float fixedAspectRatio = 9f / 16f;
     // //현재 해상도의 비율
     float currentAspectRatio = (float)Screen.width / (float)Screen.height;
@@ -18,5 +43,5 @@ public class thisCanvas : MonoBehaviour
     // // //현재 해상도 가로 비율이 더 길 경우
     // if (currentAspectRatio > fixedAspectRatio) thisCanvas.matchWidthOrHeight = 0;       
     // // //현재 해상도의 세로 비율이 더 길 경우
-    // else if (currentAspectRatio < fixedAspectRatio) thisCanvas.matchWidthOrHeight = 1;
+    // else if (currentAspectRatio < fixedAspectRatio) thisCanvas.matchWidthOrHeight = 1;*/
 }
